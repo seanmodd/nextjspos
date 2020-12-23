f3766c3defcae5b725c3dfce0fe7e8ecb29f97b2
Sean <sean@vestaridefair.com>
2020-12-23 00:27:54 -0800

    added the github version of react-admin from antonio papa and successfully ran the docker-compose yaml file independently of that beforehand but not the docker-compose backend sh ./start/sh command

---
+++ react-admin-main/src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import configureStore from "./redux/configureStore";
+import {Provider} from "react-redux";
+
+axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+
+const store = configureStore();
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
